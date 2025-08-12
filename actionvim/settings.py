f0407a6d2384429cc0b3dee0817879955056209f
Santosh Kumar Patro <hey@santoshkpatro.in>
2025-08-12 21:40:29 +0530

    feat: update index view and template for improved frontend integration

--- actionvim/settings.py
+++ actionvim/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = "actionvim.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates", BASE_DIR / "frontend"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- actionvim/urls.py
+++ actionvim/urls.py
@@ -2,14 +2,16 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.shortcuts import render
+from django.http import FileResponse
 from rest_framework.routers import SimpleRouter
 
 
 def index(request):
+    index_path = settings.BASE_DIR / "frontend" / "index.html"
     if settings.DEBUG:
-        return render(request, "index_dev.html")
-    else:
-        return render(request, "index.html", status=404)
+        return render(request, "index.html")
+
+    return FileResponse(open(index_path, "rb"), content_type="text/html")
 
 
 from actionvim.accounts.views import AccountViewSet
